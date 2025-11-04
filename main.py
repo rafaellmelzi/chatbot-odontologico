6ffca8bc6e36f77f5cd0d649393cf07ca296954c
Rafael Luiz Melzi Rossi <rafael.melzi@Citel.cloud>
2025-11-04 12:00:16 -0300

    Use: Environment variables from .env

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import PlainTextResponse
 from twilio.rest import Client
-from dotenv import load_dotenv
 from urllib.parse import parse_qs
+from dotenv import load_dotenv
 
 load_dotenv()
-
-app = FastAPI()
-app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 
 TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_WHATSAPP_NUMBER = os.getenv('TWILIO_WHATSAPP_NUMBER')
 
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
+
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-@app.get('/') 
+@app.get('/')
 async def root():
     return {'status': 'online'}
 
@@ -35,5 +35,6 @@ async def whatsapp_webhook(request: Request):
         
         msg = client.messages.create(from_=TWILIO_WHATSAPP_NUMBER, body='Ola! Bem-vindo!', to=sender)
         return PlainTextResponse('OK')
-    except:
+    except Exception as e:
+        print(f'Error: {e}')
         return PlainTextResponse('OK')
